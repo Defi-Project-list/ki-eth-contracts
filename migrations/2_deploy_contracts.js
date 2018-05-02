227453458905bc8482fad58b759111255e11f447
Tal Asa <email.tal.asa@gmail.com>
2018-05-02 17:39:16 +0300

    set gas limit for contract creation

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ var Sanity = artifacts.require("./Sanity.sol");
 var Backup = artifacts.require("./Backup.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Sanity);
-  deployer.deploy(Backup);
+  deployer.deploy(Sanity, { gas: 4712388 });
+  deployer.deploy(Backup, { gas: 4712388 });
 };
