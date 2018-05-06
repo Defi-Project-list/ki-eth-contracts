f4ae2e8f9146a05142162649647d46c36e7cc7f0
Tal Asa <email.tal.asa@gmail.com>
2018-05-06 15:18:41 +0300

    add heartbeat to sendEther

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -6,7 +6,6 @@ contract Wallet is Heritable {
 
     event GotEther(address indexed from, uint256 value);
     event SentEther(address indexed to, uint256 value);
-    event Touch();
 
     modifier logPayment {
         if (msg.value > 0) {
@@ -18,10 +17,16 @@ contract Wallet is Heritable {
     constructor() Heritable(100000) public {
     }
 
+    function sendEther(address _to, uint256 _value) public onlyOwner() {
+        require(address(this).balance <= _value);
+        heartbeat();
+        _to.transfer(_value);
+    }
+
     function getBalnace() view public returns (uint256) {
         return address(this).balance;
     }
 
-    function() payable logPayment public {
+    function() payable logPayment() public {
     }
 }
