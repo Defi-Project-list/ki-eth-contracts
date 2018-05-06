e2e6b59ac23e727d756020ce2a988261822a44bd
Tal Asa <email.tal.asa@gmail.com>
2018-05-06 19:04:54 +0300

    add 1 finney to wallet migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,5 +5,9 @@ var Wallet = artifacts.require("./Wallet.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Sanity, { gas: 4712388 });
-  deployer.deploy(Wallet, { gas: 4712388, value: web3.toWei(1, 'ether') });
-};
+  deployer.deploy(Wallet, {
+    gas: 4712388,
+    value: web3.toWei(1, 'finney')
+  });
+}
+
