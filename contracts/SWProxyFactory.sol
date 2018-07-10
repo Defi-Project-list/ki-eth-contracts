03b07ad7807ff3a4202bbd7405e34d4bf4310b72
Tal Asa <email.tal.asa@gmail.com>
2018-07-10 21:09:43 +0300

    versioning system added to swproxyfactory

--- contracts/SWProxyFactory.sol
+++ contracts/SWProxyFactory.sol
@@ -3,10 +3,18 @@ pragma solidity 0.4.24;
 import "./SWProxy.sol";
 
 contract SWProxyFactory {
-    address public swProxy;
-    address public lastver;
-    mapping(address => address) private smartwallets;
-    mapping(bytes8 => address) private versions;
+    SWProxy public swProxy;
+
+    struct SmartWallet {
+        address addr;
+        bool owner;
+    }
+    mapping(address => SmartWallet) private accounts_smartwallet;
+    mapping(address => bytes8) private smartwallets_version;
+    mapping(bytes8 => address) private versions_code;
+
+    bytes8 private production_version;
+    address private production_version_code;
 
     event SWCreated(address indexed target, address clone);
 
@@ -30,41 +38,44 @@ contract SWProxyFactory {
     }
 
     function setTarget(bytes8 _id) external {
-        address _target = versions[_id];
-        require(_target != address(0));
+        address _code = versions_code[_id];
+        require(_code != address(0));
         address _owner = SWProxy(msg.sender).owner();
-        require(msg.sender == smartwallets[_owner]);
-        SWProxy(msg.sender).init(_owner, _target);
+        SmartWallet storage _sw = accounts_smartwallet[_owner];
+        require(msg.sender == _sw.addr && _sw.owner == true);
+        SWProxy(msg.sender).init(_owner, _code);
     }
 
     function addVersion(bytes8 _id, address _target) public {
         require(_target != address(0));
-        require(versions[_id] == address(0));
-        versions[_id] = _target;
-        lastver = _target;
+        address _code = versions_code[_id];
+        require(_code == address(0));
+        versions_code[_id] = _target;
+        production_version = _id;
+        production_version_code = _target;
     }
 
     function fixme() public {
-        address _sw = smartwallets[msg.sender];
-        require(_sw != address(0));
-        (SWProxy(_sw)).init(msg.sender, 0x0);
+        //address _sw = smartwallets[msg.sender];
+        //require(_sw != address(0));
+        //(SWProxy(_sw)).init(msg.sender, 0x0);
     }
 
     function getSmartWallet(address _account) public view returns (address) {
-        return smartwallets[_account];
+        return accounts_smartwallet[_account].addr;
     }
 
     function createSmartWallet() public returns (address) {
-        require(lastver != address(0));
-        address _sw = smartwallets[msg.sender];
-        if (_sw == address(0)) {
-            _sw = _createSmartWallet(address(this), swProxy);
-            require(_sw != address(0));
-            smartwallets[msg.sender] = _sw;
-            (SWProxy(_sw)).init(msg.sender, lastver);
-            emit SWCreated(msg.sender, _sw);
+        require(production_version_code != address(0));
+        SmartWallet storage _sw = accounts_smartwallet[msg.sender];
+        if (_sw.addr == address(0)) {
+            _sw.addr = _createSmartWallet(address(this), swProxy);
+            require(_sw.addr != address(0));
+            _sw.owner = true;
+            (SWProxy(_sw.addr)).init(msg.sender, production_version_code);
+            emit SWCreated(msg.sender, _sw.addr);
         }
-        return _sw;
+        return _sw.addr;
     }
 }
 
