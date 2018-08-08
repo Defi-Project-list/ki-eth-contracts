f13e1ad31e0b24d90bfa4687ebe8566dc7763dfd
Tal Asa <email.tal.asa@gmail.com>
2018-08-08 22:08:38 +0300

    .

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,16 +9,18 @@ var SmartWallet = artifacts.require("./SmartWallet.sol");
 var SmartWallet2 = artifacts.require("./test/SmartWallet2.sol");
 var Root = artifacts.require("./Root.sol");
 
-const liveNetworks = { rinkeby: true, kovan: true }
+const liveNetworks = { rinkeby: true, kovan: true };
+const gasPrice =  web3.toWei(1, 'gwei');
+const gas = 5912388;
 
 module.exports = function(deployer, network) {
   deployer.then(async () => {
-	  const factoryProxy = await deployer.deploy(SW_FactoryProxy, { gas: 5912388 , overwrite: !liveNetworks[network] });
-  	  const factory = await deployer.deploy(SW_Factory, { gas: 5912388 });
-	  await factoryProxy.setTarget(factory.address);
-  	  const sw = await deployer.deploy(SmartWallet, { gas: 5912388 });
+	  const factoryProxy = await deployer.deploy(SW_FactoryProxy, { gas, gasPrice, overwrite: !liveNetworks[network] });
+  	  const factory = await deployer.deploy(SW_Factory, { gas, gasPrice });
+	  await factoryProxy.setTarget(factory.address, { gas, gasPrice });
+  	  const sw = await deployer.deploy(SmartWallet, { gas, gasPrice });
 	  try { 
-	  	await SW_Factory.at(factoryProxy.address).addVersion(sw.address);
+	  	await SW_Factory.at(factoryProxy.address).addVersion(sw.address, { gas, gasPrice });
 	  }
 	  catch(err) {
 		console.error('addVersion failed. Please check version number.');
