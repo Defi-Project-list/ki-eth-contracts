78903c84f9ac74b1214188402da15f340559a569
Tal Asa <tal@kirobo.io>
2018-12-12 20:48:11 +0200

    optimize contract size

--- truffle-config.js
+++ truffle-config.js
@@ -8,7 +8,8 @@ let mnemonic = "front assume robust donkey senior economy maple enhance click br
 const devNetwork = {
   host: "127.0.0.1",	
   port: 8545,
-  network_id: "*"
+  network_id: "*",
+  gas: 7000000
 };
 
 module.exports = {
@@ -21,7 +22,8 @@ module.exports = {
           mnemonic, "https://rinkeby.infura.io/Mi3WQKlqLIU6IQtAvddB"
         );
       },
-      network_id: "4"
+      network_id: "4",
+      gas: 7000000
     },
     kovan: {
       provider: function() {
@@ -31,7 +33,8 @@ module.exports = {
           mnemonic, "https://kovan.infura.io/Mi3WQKlqLIU6IQtAvddB"
         );
       },
-      network_id: "42"
+      network_id: "42",
+      gas: 7000000
     },
     ropsten: {
       provider: function() {
@@ -41,15 +44,23 @@ module.exports = {
           mnemonic, "https://ropsten.infura.io/Mi3WQKlqLIU6IQtAvddB"
         );
       },
-      network_id: "3"
+      network_id: "3",        
+      gas: 7000000
     },
     ganache: {
       provider: function() {
         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
       },
       network_id: "1337",
+      gas: 7000000
     },
     development: devNetwork,
     dev: devNetwork
-  }
+  },
+  solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    }
 };
