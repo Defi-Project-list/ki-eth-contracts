6eb6a9ea5fc889708f370f5f5fccb37ad2173b51
Tal Asa <email.tal.asa@gmail.com>
2021-05-23 20:03:47 +0300

    making multisig more secured

--- contracts/lib/MultiSig.sol
+++ contracts/lib/MultiSig.sol
@@ -1,74 +1,101 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 pragma solidity ^0.8.0;
 pragma abicoder v1;
 
 abstract contract MultiSig {
-    mapping(address => bool) private s_owners;
+    mapping(address => bool) s_owners;
+
+    address s_markedForRemoval;
 
     struct Action {
         address owner;
         uint256 value;
-        bytes data;
+        uint256 height;
+        bytes32 data;
     }
 
-    Action private s_action;
+    Action public s_action;
 
-    constructor(
-        address owner1,
-        address owner2,
-        address owner3
-    ) {
-        require(owner1 != address(0), "owner1 cannot be 0");
-        require(owner2 != address(0), "owner2 cannot be 0");
-        require(owner3 != address(0), "owner3 cannot be 0");
+    event MultiSigRequest(address indexed from, bytes4 indexed selector, uint256 value, bytes32 hashedData);
+    event MultiSigExecute(address indexed from, bytes4 indexed selector, uint256 value, bytes32 hashedData);
+    event MultiSigCancel(address indexed from);
+    event MultiSigReplaceOwnerCall(address indexed by, address indexed from, address indexed to);
 
-        require(owner1 != owner2, "owner1 cannot be owner2");
-        require(owner2 != owner3, "owner2 cannot be owner3");
-        require(owner1 != owner3, "owner1 cannot be owner3");
-
+    constructor(address owner1, address owner2, address owner3) {
+        require(owner1 != address(0), "MultiSig: owner1 cannot be 0");
+        require(owner2 != address(0), "MultiSig: owner2 cannot be 0");
+        require(owner3 != address(0), "MultiSig: owner3 cannot be 0");
+        require(owner1 != owner2, "MultiSig: owner1 cannot be owner2");
+        require(owner2 != owner3, "MultiSig: owner2 cannot be owner3");
+        require(owner1 != owner3, "MultiSig: owner1 cannot be owner3");
         s_owners[owner1] = true;
         s_owners[owner2] = true;
         s_owners[owner3] = true;
     }
 
-    modifier multiSig2of3(uint256 value) {
-        require(s_owners[msg.sender], "only owners");
-
-        if (s_action.owner == address(0)) {
-            s_action.owner = msg.sender;
-            s_action.data = msg.data;
-            s_action.value = value;
-            return;
-        }
-
-        require(s_action.owner != msg.sender, "same owner cannot sign twice");
-        require(s_action.value == value, "must sign the same value");
-        require(
-            keccak256(s_action.data) == keccak256(msg.data),
-            "must sign the same data"
-        );
-
-        s_action.owner = address(0);
-        _;
+    modifier multiSig2of3 (uint256 value) {
+      require(s_owners[msg.sender] && msg.sender != s_markedForRemoval, 'MultiSig: only owners that are not being removed');
+      bytes32 hashedData = keccak256(msg.data);
+      s_action.height = block.number;
+      if (s_action.owner == address(0)) {
+          s_action.owner = msg.sender;
+          s_action.data = hashedData;
+          s_action.value = value;
+          emit MultiSigRequest(msg.sender, sig(msg.data), value, hashedData);
+          return;
+      }
+      require(s_action.owner != msg.sender, 'MultiSig: same owner cannot sign twice');
+      require(s_action.value == value, 'MultiSig: must sign the same value');
+      require(s_action.data == hashedData, 'MultiSig: must sign the same data');
+      s_action.owner = address(0);
+      emit MultiSigExecute(msg.sender, sig(msg.data), value, hashedData);
+      _;
     }
 
-    function isOwner() public view returns (bool) {
-        return s_owners[msg.sender];
+    function cancel() external {
+      require(s_owners[msg.sender], 'MultiSig: only owners can cancel');
+      require(s_markedForRemoval != msg.sender, 'MultiSig: only owners that are not being replaced can cancel');
+      s_action.owner = address(0);
+      s_markedForRemoval = address(0);
+      emit MultiSigCancel(msg.sender);
     }
 
-    function cancel() public {
-        require(s_owners[msg.sender], "only owners");
-        s_action.owner = address(0);
+    function replaceOwner(address owner, address newOwner) external {
+      require(owner != address(0), 'MultiSig: owner cannot be 0');
+      require(newOwner != address(0), 'MultiSig: new Owner cannot be 0');
+      require(s_owners[owner] == true, 'MultiSig: owner must exist');
+      require(owner != msg.sender, "MultiSig: senders cannot replace themselves");
+      require(s_owners[newOwner] == false, 'MultiSig: new owner must not exist');
+      s_markedForRemoval = owner;
+      emit MultiSigReplaceOwnerCall(msg.sender, owner, newOwner);
+      _replaceOwner(owner, newOwner);
     }
 
-    function replaceOwner(address owner, address newOwner)
-        public
-        multiSig2of3(0)
+    function isOwner() external view returns (bool) {
+      return s_owners[msg.sender];
+    }
+
+    function sig(bytes calldata data) pure public returns (bytes4) {
+      if (data.length < 4) {
+        return bytes4(0);
+      }
+      return(
+        data[0] |
+        (bytes4(data[1]) >> 8) |
+        (bytes4(data[2]) >> 16) |
+        (bytes4(data[3]) >> 24)
+      );
+    }
+
+    function _replaceOwner(address owner, address newOwner)
+      private 
+      multiSig2of3(0)
     {
-        require(s_owners[owner] == true, "owner should exist");
-        require(s_owners[newOwner] == false, "new owner should not exist");
-        s_owners[owner] = false;
-        s_owners[newOwner] = true;
+      s_owners[owner] = false;
+      s_owners[newOwner] = true;
+      s_markedForRemoval = address(0);
     }
+
+
 }
