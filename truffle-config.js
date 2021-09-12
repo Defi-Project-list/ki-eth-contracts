b63f688a2057dac8c15d05e98113c8a21ac32d03
Ori Shalom <oris@kirobo.io>
2021-09-12 17:29:11 +0300

    fix run of tests

--- truffle-config.js
+++ truffle-config.js
@@ -65,9 +65,9 @@ module.exports = {
     ganache: {
 	    host: "127.0.0.1",
       port: 8545,
-      provider: function() {
+      /* provider: function() {
          return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
-      },
+      }, */
       network_id: "*",
       gas,
       ens: {
